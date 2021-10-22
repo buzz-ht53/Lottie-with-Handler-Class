7c310af54b90f4f6e1bc3e4e6cf78360123fe740
buzz-ht53 <akshayr.java@gmail.com>
2021-10-22 13:34:08 +0530

    First Commit

---
+++ app/src/main/java/com/buzz_ht/lottieanimations/MainActivity.java
@@ -0,0 +1,36 @@
+package com.buzz_ht.lottieanimations;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+import android.view.Window;
+import android.view.WindowManager;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        requestWindowFeature(Window.FEATURE_NO_TITLE);
+        getSupportActionBar().hide();
+        this.getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
+        setContentView(R.layout.activity_main);
+
+        Handler handler = new Handler();
+
+        handler.postDelayed(new Runnable() {
+            @Override
+            public void run() {
+
+                startActivity(new Intent(getApplicationContext(),MainActivity2.class));
+
+
+            }
+        },10000);
+
+
+
+    }
+}
